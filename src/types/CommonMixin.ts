97960c4007f7b984c9f021264daf3329824432d1
Kamil <majcherczak.kamil@protonmail.com>
2024-02-29 21:57:25 +0100

    api types

---
+++ src/types/CommonMixin.ts
@@ -0,0 +1,5 @@
+export interface CommonMixin {
+  created: Date;
+  edited: Date;
+  url: string;
+}
---
+++ src/types/Person.ts
@@ -0,0 +1,17 @@
+import { CommonMixin } from './CommonMixin';
+
+export interface Person extends CommonMixin {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+}
