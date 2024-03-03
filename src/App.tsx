1f6d2c238b04e3a878d2878d46d0f8f8674fed7f
Kamil <majcherczak.kamil@protonmail.com>
2024-03-03 22:00:03 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
-import './App.scss';
+import { People } from './components/People/People';
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { HomePage } from './components/HomePage/HomePage';
 
 function App() {
-  return <div className='App'></div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Link to="/people">People page</Link>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<Navigate to="/" replace={true} />} />
+          <Route path="/people" element={<People />}>
+            <Route path=":id" element={<People />} />
+          </Route>
+          <Route path="/vehicles" element={<People />}>
+            <Route path=":id" element={<People />} />
+          </Route>
+          <Route path="/starships" element={<People />}>
+            <Route path=":id" element={<People />} />
+          </Route>
+          <Route path="/species" element={<People />}>
+            <Route path=":id" element={<People />} />
+          </Route>
+          <Route path="/planets" element={<People />}>
+            <Route path=":id" element={<People />} />
+          </Route>
+          <Route path="/films" element={<People />}>
+            <Route path=":id" element={<People />} />
+          </Route>
+        </Routes>
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
