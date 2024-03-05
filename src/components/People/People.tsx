046f2af6cc85e768e4a9639be9ef6de1d165be44
Kamil <majcherczak.kamil@protonmail.com>
2024-03-05 22:00:48 +0100

    People component

---
+++ src/components/People/People.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useFetchData } from '../../hooks/useFetchData';
+import { ListOf } from '../../types/ListOf';
+import { Person } from '../../types/Person';
+import { Link, useLocation } from 'react-router-dom';
+
+export const People = () => {
+  const { pathname } = useLocation();
+  const { data, isLoading, error, refetch } =
+    useFetchData<ListOf<Person>>(pathname);
+
+  if (error) {
+    return (
+      <div>
+        Error
+        <button
+          onClick={() => {
+            refetch();
+          }}
+          type="button"
+        >
+          Try again
+        </button>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {data?.results?.map((person) => {
+        return (
+          <Link
+            to={person.url.replace('https://swapi.dev/api', '')}
+            key={person.url}
+          >
+            {person.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
