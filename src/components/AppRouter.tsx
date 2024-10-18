f3c320f8c36513e7334737e704eac44c1b6bce67
Kamil <102466723+kuzgan@users.noreply.github.com>
2024-10-18 23:39:36 +0200

    Router: Routes update

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -3,6 +3,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from './HomePage/HomePage';
 import { People } from './People/People';
 import { PersonPage } from './People/PersonPage/PersonPage';
+import { Vehicles } from './Vehicles/Vehicles';
+import { VehiclePage } from './Vehicles/VehiclePage/VehiclePage';
+import { Starships } from './starships/starships';
+import { StarshipPage } from './starships/starshipPage/starshipPage';
+import { Planets } from './Planets/Planets';
+import { PlanetPage } from './Planets/PlanetPage/PlanetPage';
+import { Films } from './Films/Films';
+import { FilmPage } from './Films/FilmPage/FilmPage';
 
 export const AppRouter = () => {
   return (
@@ -12,16 +20,16 @@ export const AppRouter = () => {
       <Route path="*" element={<Navigate to="/" replace={true} />} />
       <Route path="/people" element={<People />} />
       <Route path="/people/:id" element={<PersonPage />} />
-      <Route path="/vehicles" element={<People />} />
-      <Route path="/vehicles/:id" element={<People />} />
-      <Route path="/starships" element={<People />} />
-      <Route path="/starships/:id" element={<People />} />
-      <Route path="/species" element={<People />} />
+      <Route path="/vehicles" element={<Vehicles />} />
+      <Route path="/vehicles/:id" element={<VehiclePage />} />
+      <Route path="/starships" element={<Starships />} />
+      <Route path="/starships/:id" element={<StarshipPage />} />
+      <Route path="/species" element={<People />} /> {/* needs to be change */}
       <Route path="/species/:id" element={<People />} />
-      <Route path="/planets" element={<People />} />
-      <Route path="/planets/:id" element={<People />} />
-      <Route path="/films" element={<People />} />
-      <Route path="/films/:id" element={<People />} />
+      <Route path="/planets" element={<Planets />} />
+      <Route path="/planets/:id" element={<PlanetPage />} />
+      <Route path="/films" element={<Films />} />
+      <Route path="/films/:id" element={<FilmPage />} />
     </Routes>
   );
 };
