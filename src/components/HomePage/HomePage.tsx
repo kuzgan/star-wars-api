283d5d460516cfc9e14290a4ca7f08389973766e
Kamil <102466723+kuzgan@users.noreply.github.com>
2024-10-21 20:19:13 +0200

    changed links

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom';
 export const HomePage = () => {
   return (
     <nav className="home">
-      <Link className="home__button" to="/people">
+      <Link className="home__button" to="/people/?page=1">
         People
       </Link>
-      <Link className="home__button" to="/planets">
+      <Link className="home__button" to="/planets/?page=1">
         Planets
       </Link>
-      <Link className="home__button" to="/films">
+      <Link className="home__button" to="/films/?page=1">
         Films
       </Link>
-      <Link className="home__button" to="/starships">
+      <Link className="home__button" to="/starships/?page=1">
         Starships
       </Link>
-      <Link className="home__button" to="/vehicles">
+      <Link className="home__button" to="/vehicles/?page=1">
         Vehicles
       </Link>
-      <Link className="home__button" to="/species">
+      <Link className="home__button" to="/species/?page=1">
         Species
       </Link>
     </nav>
