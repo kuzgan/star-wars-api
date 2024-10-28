4c9b5646de264bc3594416bf57e453625c3cb71d
Kamil <102466723+kuzgan@users.noreply.github.com>
2024-10-28 20:30:20 +0100

    src change

--- src/components/Planets/PlanetPage/PlanetPage.tsx
+++ src/components/Planets/PlanetPage/PlanetPage.tsx
@@ -33,7 +33,7 @@ export const PlanetPage = () => {
       ) : (
         <div>
           <img
-            src={`/images/characters/${id}.jpg`}
+            src={`/images/planets/${id}.jpg`}
             onError={({ currentTarget }) => {
               currentTarget.onerror = null;
               currentTarget.src = '/images/big-placeholder.jpg';
