2be61cb87b67ec3e0d1f3a19a516f6f6e7fe55bf
Kamil <102466723+kuzgan@users.noreply.github.com>
2025-05-28 16:06:23 +0200

    initial breadcrumbs style

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './breadcrumbs.scss';
 
 interface BreadcrumbsProps {
   previous?: string;
@@ -11,12 +12,22 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = () => {
   const crumbs = location.pathname.split('/').filter((crumb) => crumb !== '');
 
   return (
-    <nav>
-      {crumbs.length === 0 ? <span>home</span> : <Link to="/">home</Link>}
-      {crumbs.length > 0 && <span>/</span>}
+    <nav className="breadcrumbs">
+      {crumbs.length === 0 ? (
+        <span className="breadcrumbs__item">home</span>
+      ) : (
+        <Link to="/" className="breadcrumbs__link">
+          home
+        </Link>
+      )}
+      {/* {crumbs.length > 0 && <span>/</span>} */}
       {crumbs.length === 1 && <span>{crumbs[0]}</span>}
-      {crumbs.length === 2 && <Link to={`/${crumbs[0]}`}>{crumbs[0]}</Link>}
-      {crumbs.length === 2 && <span>/</span>}
+      {crumbs.length === 2 && (
+        <Link to={`/${crumbs[0]}`} className="breadcrumbs__link">
+          {crumbs[0]}
+        </Link>
+      )}
+      {/* {crumbs.length === 2 && <span>/</span>} */}
       {crumbs.length === 2 && <span>{crumbs[1]}</span>}
     </nav>
   );
