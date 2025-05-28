a2fa79b1dc4c57ae1ccd360d88f7ffde675021d6
Kamil <102466723+kuzgan@users.noreply.github.com>
2025-05-28 16:07:02 +0200

    style: app setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AppRouter } from './components/AppRouter';
 import { Breadcrumbs } from './components/Breadcrumbs/Breadcrumbs';
+import '../src/sass/layouts/app.scss';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
+      <div className="app">
         <Breadcrumbs />
         <AppRouter />
       </div>
