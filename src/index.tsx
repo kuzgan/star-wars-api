9dca6c75cb46c64039cb55a26ed0345d7658fd72
Kamil <102466723+kuzgan@users.noreply.github.com>
2025-12-08 20:51:22 +0100

    style import fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import '../src/sass/style.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
